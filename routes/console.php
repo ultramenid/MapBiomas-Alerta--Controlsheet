0f9df19eb8e9648c6ce9db791f451727b0a4d8a8
ultramenid <malichamdan@gmail.com>
2026-03-12 20:02:40 +0700

    remove it

--- routes/console.php
+++ routes/console.php
@@ -31,9 +31,3 @@ Schedule::command('alerts:cleanup-orphaned-images')
     ->runInBackground()
     ->appendOutputTo(storage_path('logs/cleanup-orphaned.log'));
 
-// Optimize tabel alerts setiap hari jam 3 pagi untuk reclaim ruang setelah migrasi base64
-Schedule::command('db:optimize-tables --tables=alerts')
-    ->dailyAt('03:00')
-    ->withoutOverlapping()
-    ->runInBackground()
-    ->appendOutputTo(storage_path('logs/optimize-tables.log'));
