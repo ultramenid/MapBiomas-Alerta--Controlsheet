9642df3f2ba2eb95c734133ce70c9517c7ec80e1
ultramenid <malichamdan@gmail.com>
2026-08-17 14:06:44 +0700

    migration mode

---
+++ resources/views/migration.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Migration on progress</title>
+</head>
+<body>
+    Controlsheet is under maintenance, please wait for a while. We are migrating the data to the new system. Thank you for your patience.
+</body>
+</html>
